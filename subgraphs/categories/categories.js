e84293c33097783c3bbcf3c7b85c26d1aff2e39b
Octanium <octaniumcontact@gmail.com>
2021-10-03 14:23:24 +0200

    :sparkles:  Setup categories subgraph

---
+++ subgraphs/categories/categories.js
@@ -0,0 +1,39 @@
+// Open Telemetry (optional)
+const { ApolloOpenTelemetry } = require('supergraph-demo-opentelemetry');
+
+if (process.env.APOLLO_OTEL_EXPORTER_TYPE) {
+  new ApolloOpenTelemetry({
+    type: 'subgraph',
+    name: 'categories',
+    exporter: {
+      type: process.env.APOLLO_OTEL_EXPORTER_TYPE, // console, zipkin, collector
+      host: process.env.APOLLO_OTEL_EXPORTER_HOST,
+      port: process.env.APOLLO_OTEL_EXPORTER_PORT,
+    }
+  }).setupInstrumentation();
+}
+
+const { ApolloServer, gql } = require('apollo-server');
+const { buildFederatedSchema } = require('@apollo/federation');
+const { readFileSync } = require('fs');
+
+const port = process.env.APOLLO_PORT || 4000;
+
+const categories = [
+  { _id: '1' }
+]
+
+const typeDefs = gql(readFileSync('./categories.graphql', { encoding: 'utf-8' }));
+const resolvers = {
+  Category: {
+        __resolveReference: (reference) => {
+          console.log('categories.Category.__resolveReference', reference)
+            return users.find(u => u.email == reference.email);
+        }
+    }
+}
+
+const server = new ApolloServer({ schema: buildFederatedSchema({ typeDefs, resolvers }) });
+server.listen( {port: port} ).then(({ url }) => {
+  console.log(`🚀 Users subgraph ready at ${url}`);
+}).catch(err => {console.error(err)});
